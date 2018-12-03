e7aa01d5b02db1850c221bcb67de264f8396870b
Dan Rusu <dan.rusu@azets.com>
2018-12-03 17:25:37 +0200

    matrix example

---
+++ src/fundamentals/base/Matrix.java
@@ -0,0 +1,29 @@
+package fundamentals.base;
+
+public class Matrix {
+
+    public static void main(String[] args) {
+
+        int LINES_NR = 5;
+        int COLUMNS_NR = 5;
+
+        String[][] a = new String[LINES_NR][COLUMNS_NR];
+
+        // initialize elements
+        for (int i = 0; i < LINES_NR; i++) {
+            for (int j = 0; j < COLUMNS_NR; j++) {
+
+                a[i][j] = "" + i + j;
+            }
+        }
+
+        // print elements
+        for (int i = 0; i < LINES_NR; i++) {
+            for (int j = 0; j < COLUMNS_NR; j++) {
+
+                System.out.printf("%s ", a[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
