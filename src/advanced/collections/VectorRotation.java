5b9fa52e6b9631a6392a7e7247bb0d04d903ec1b
Dan Rusu <dan.rusu@azets.com>
2018-12-03 15:29:20 +0200

    vector rotation

--- src/advanced/collections/VectorRotation.java
+++ src/advanced/collections/VectorRotation.java
@@ -10,10 +10,10 @@ public class VectorRotation {
 
         Vector<Integer> v = new Vector<>();
 
-        v.addAll(List.of(1, 2, 3, 4, 5, 6));
+        v.addAll(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12));
 
 
-        System.out.println(rotateRight(v, 12));
+        System.out.println(rotateRight(v, 145));
 
     }
 
