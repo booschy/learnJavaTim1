aef1e957de90eafc7e1be43c3e5a6920c3a53ced
Dan Rusu <dan.rusu@azets.com>
2018-12-03 15:03:39 +0200

    vector rotation

---
+++ src/advanced/collections/VectorRotation.java
@@ -0,0 +1,48 @@
+package advanced.collections;
+
+import java.util.List;
+import java.util.Vector;
+
+public class VectorRotation {
+
+
+    public static void main(String[] args) {
+
+        Vector<Integer> v = new Vector<>();
+
+        v.addAll(List.of(1, 2, 3, 4, 5, 6));
+
+
+        System.out.println(rotateRight(v, 12));
+
+    }
+
+
+    public static Vector<Integer> rotateRight(Vector<Integer> v, int rotationsCount){
+
+        Vector<Integer> finalVector = new Vector<>();
+        finalVector.addAll(v);
+
+        // rotate rotationsCount times
+        for(int i=0; i < rotationsCount % v.size(); i++){
+            finalVector = rotateRightOnce(finalVector);
+        }
+
+        return finalVector;
+    }
+
+
+    public static Vector<Integer> rotateRightOnce(Vector<Integer> v){
+
+        Vector<Integer> finalVector = new Vector<>();
+
+        // copy elements from index 1 .. size-1
+        finalVector.addAll(v.subList(0, v.size()-1));
+
+        // put the last element on index 0
+        finalVector.add(0, v.get(v.size()-1));
+
+        return finalVector;
+    }
+
+}
